429313b6b76d284b5aa355579c9a68b74e851cc4
ashvinnagarajan <ashvinagarajan@gmail.com>
2019-08-10 09:50:56 -0700

    formatting improvements

--- photos.js
+++ photos.js
@@ -1,8 +1,19 @@
 var questions = [
-    {title: "title1", image: "img/img1.jpg", body: "hey1", answer: "hi"},
+    {title: "", image: "img/img1.jpg", body: "hey1", answer: "hi"},
     {title: "title2", image: "img/img2.jpg", body: "hey2", answer: "hey"},
     {title: "title3", image: "img/img3.jpg", body: "hey3", answer: "whatsup"},
     {title: "title4", image: "img/img4.jpg", body: "hey4", answer: "howdy"},
+    {title: "title5", image: "img/img5.jpg", body: "hey1", answer: "hi"},
+    {title: "title6", image: "img/img6.jpg", body: "hey2", answer: "hey"},
+    {title: "title7", image: "img/img7.jpg", body: "hey3", answer: "whatsup"},
+    {title: "title8", image: "img/img8.jpg", body: "hey4", answer: "howdy"},
+    {title: "title9", image: "img/img9.jpg", body: "hey1", answer: "hi"},
+    {title: "title10", image: "img/img10.jpg", body: "hey2", answer: "hey"},
+    {title: "title11", image: "img/img11.jpg", body: "hey3", answer: "whatsup"},
+    {title: "title12", image: "img/img12.jpg", body: "hey4", answer: "howdy"},
+    {title: "title13", image: "img/img13.jpg", body: "hey2", answer: "hey"},
+    {title: "title14", image: "img/img14.jpg", body: "hey3", answer: "whatsup"},
+    {title: "title15", image: "img/img15.jpg", body: "hey4", answer: "howdy"},
 ]
 
 var numQ = -1;
